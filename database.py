188483aa534da9bbc0cf85d79995c648bdfb1e75
Nicolas Pieroni <pieroni.nico@gmail.com>
2024-10-02 04:53:52 -0300

    MOD(Cambio la implementación para inserción de datos (la responsibilidad se delego a la clase classes.Database))

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-import pyodbc   
+from classes import Database
 import json
 
 #Leer el archivo json
@@ -23,17 +23,5 @@ connection_string = (
     f"Trusted_Connection={config['database']['Trusted_Connection']};"
 )
 
-def insertar_dato_en_db(dato):
-    qr=config['numeroQr']
-    #Abro conexion
-    conn=pyodbc.connect(connection_string)
-    cursor=conn.cursor()
-    #Defino el query
-    query="INSERT INTO Qrs (numeroQr, charlaN, apellidoNombre, legajo, asistencia, fechaInscripcion) VALUES (?,?,?,?,?,?);"
-    cursor.execute(query, (qr, dato["charlaN"], dato["apellidoNombre"], dato["legajo"], 0, dato["fecha"]))
-    conn.commit()
-    cursor.close()
-    conn.close()
-    config["numeroQr"]=qr+1
-    with open('config.json', 'w') as archivo:
-        json.dump(config, archivo, indent=4)
+def obtener_database() -> Database:
+    return Database(connection_string)
